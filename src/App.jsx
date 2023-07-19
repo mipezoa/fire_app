e07c40b9c9bd3237fc72acf9b33d1737e2024b29
mipezoa <mipezoa@uc.cl>
2023-07-19 11:28:28 -0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,97 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { MapContainer, TileLayer, Marker, Popup, LayersControl } from "react-leaflet";
 import L from 'leaflet';
 import { data } from "./data";
 
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTemperatureThreeQuarters, faDroplet, faWind } from "@fortawesome/free-solid-svg-icons";
 
 
-const { BaseLayer, Overlay } = LayersControl;
+const { BaseLayer } = LayersControl;
+
 const App = () => {
   const position = [-33.505, -70];
   const riesgoFireData = data;
-  const [displayProperty, setDisplayProperty] = useState("");
+  const [displayProperty, setDisplayProperty] = useState("humedad");
+  const [percentage, setPercentage] = useState(0);
+  const [indicatorClass, setIndicatorClass] = useState("");
 
-  const customizeIcon = (feature, displayProperty) => {
-    const size = 30;
-    // Define the fire risk thresholds
-    const color = displayProperty === "temperatura" && feature.properties.temperatura > 25 ? "red"
-      : temperatura > 25 || humedad < 30 || intensidadDelViento > 30 ? "yellow"
-        : temperatura > 25 && humedad < 30 || intensidadDelViento > 30 ? "orange"
-          : "green";
-    const style = `background-color: ${color}; width: ${size}px; height: ${size}px; border-radius: 80%; display: flex; justify-content: center; align-items: center; color: white ; font-weight: bold;`;
+  useEffect(() => {
+    setTimeout(() => {
+      if (displayProperty < 100) {
+        setPercentage(displayProperty + 1);
+      }
+    }, 50);
+  }, [displayProperty]);
 
-    return L.divIcon({
-      className: 'custom-icon',
-      iconSize: [size, size],
-      html: `<div style="${style}">${value}</div>`,
-    });
+  const getIndicatorClass = (value) => {
+    if (value <= 25) {
+      return "good";
+    } else if (value <= 50) {
+      return "regular";
+    } else if (value <= 75) {
+      return "Preemergency";
+    } else {
+      return "Emergency";
+    }
   };
+
+  // CSS styles or inline styles
+  const indicatorClassStyles = {
+    good: {
+      path: {
+        stroke: "green"
+      },
+      trail: {
+        stroke: "lightgreen"
+      }
+    },
+    regular: {
+      path: {
+        stroke: "yellow"
+      },
+      trail: {
+        stroke: "lightyellow"
+      }
+    },
+    Preemergency: {
+      path: {
+        stroke: "orange"
+      },
+      trail: {
+        stroke: "lightorange"
+      }
+    },
+    Emergency: {
+      path: {
+        stroke: "red"
+      },
+      trail: {
+        stroke: "lightred"
+      }
+    }
+  };
+
+  const indicatorClassStyles1 = {
+    customClass: {
+      path: {
+        stroke: "url(#gradient)",
+        strokeWidth: "8",
+        strokeLinecap: "round",
+        strokeDasharray: "180",
+        strokeDashoffset: "90",
+        PointerEvent: "true"
+      },
+      trail:{stroke:"transparent,"},
+    },
+  };
+
   const defaultIcon = (value) => {
-    const size = 30;
+    const size = 50;
     // Define the fire risk thresholds
     const colorD = value === "humedad"
       ? "blue"
@@ -52,8 +113,14 @@ const App = () => {
     temperatura: "temperatura",
     windspeed: "intensidadDelViento"
   };
+  /*Handle Buttons */
+
   const handleButtonClick = (property) => {
     setDisplayProperty(property);
+  };
+
+  const handleMarkerClick = (feature) => {
+    setSelectedMarker(feature);
   };
 
   return (
@@ -66,7 +133,7 @@ const App = () => {
       <LayersControl>
         <BaseLayer checked name="image satellite">
           <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
             url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
           />
         </BaseLayer>
@@ -93,18 +160,43 @@ const App = () => {
             icon={defaultIcon(feature.properties[displayProperty])}
           >
             <Popup>
-              <strong>{feature.properties.nombreEstacion}</strong>
-              <br />
-              {displayProperty && `${propertyDisplayNames[displayProperty]}: ${feature.properties[displayProperty]}`}
+              <div>
+                <strong>{feature.properties.nombreEstacion}</strong>
+                <br />
+                {displayProperty && `${propertyDisplayNames[displayProperty]}: ${feature.properties[displayProperty]}`}
+                <div style={{ textAlign: "center" }}>
+                  <h4>Risk Level</h4>
+                  <div style={{ width: 300, marginLeft: 0 }}>
+                    <svg>
+                      <defs>
+                        <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                          <stop offset="0%" style={{ stopColor: "red" }} />
+                          <stop offset="25%" style={{ stopColor: "orange" }} />
+                          <stop offset="50%" style={{ stopColor: "yellow" }} />
+                          <stop offset="75%" style={{ stopColor: "green" }} />
+                        </linearGradient>
+                      </defs>
+                    </svg>
+                    <CircularProgressbar
+                      value={feature.properties[displayProperty]}
+                      text={`${getIndicatorClass(feature.properties[displayProperty])}`}
+                      className={getIndicatorClass(feature.properties[displayProperty])}
+                      styles={indicatorClassStyles1.customClass}
+                    />
+                  </div>
+                </div>
+              </div>
+
             </Popup>
           </Marker>
-        ))}
+        ))
+      }
       <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
-    </MapContainer>
+    </MapContainer >
   );
 };
 
